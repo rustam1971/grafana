99e640cdb2e3778e026f6b941d36c9bf94c9a795
rustam1971 <66147096+rustam1971@users.noreply.github.com>
2026-07-02 12:48:37 +0800

    Update and rename grafana to grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 
+# Usage: wget -qO /tmp/install.sh https://raw.githubusercontent.com/rustam1971/grafana/grafana bash
 
 apt update && apt install -y apt-transport-https software-properties-common wget
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /usr/share/keyrings/grafana.gpg > /dev/null
